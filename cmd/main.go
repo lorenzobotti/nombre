5f6e6e3b4b6d028734dee557e97fd544f60ee505
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-06-02 18:57:17 +0200

    Add command line arguments

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"nombre"
 	"os"
@@ -8,19 +9,29 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	shortenOne := flag.Bool("un", false, `accorcia l'uno (esempio: "ventunmila")`)
+	spaces := flag.Bool("spazi", false, `spazi tra gli ordini di grandezza (esempio: "trentamila duecento quarantre")`)
+	flag.Parse()
+
+	options := nombre.Options{
+		SpaceAfterMinus:              *spaces,
+		SpaceBetweenOrderOfMagnitude: *spaces,
+		ShortenOne:                   *shortenOne,
+	}
+
+	if len(flag.Args()) == 0 {
 		fmt.Fprintln(os.Stderr, "i need at least one argument")
 		os.Exit(1)
 	}
 
-	for _, arg := range os.Args[1:] {
+	for _, arg := range flag.Args() {
 		number, err := strconv.Atoi(arg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "\"%s\" is not a valid number\n", arg)
 			os.Exit(1)
 		}
 
-		formatted := nombre.Convert(number)
+		formatted := nombre.ConvertWithOptions(number, options)
 		fmt.Println(formatted)
 	}
 }
